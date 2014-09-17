7de6156cc5830d9e6db7342fc4f96f67886bbd83
ZF2 <zf2@zf2-VirtualBox.(none)>
2014-09-17 16:33:24 +0200

    3

--- config/application.config.php
+++ config/application.config.php
@@ -2,10 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        
+        'ZendDeveloperTools',
+
         // our app modules
         'Application',
-        
+
     ),
 
     // These are various options for the listeners attached to the ModuleManager
